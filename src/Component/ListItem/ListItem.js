06c6c4182d876f4bdbb2d1daec0b4aa62f00c0a7
AhmedHassan588 <syedahmedhassan588@gmail.com>
2023-02-22 19:28:45 +0500

    update-list-item

--- src/Component/ListItem/ListItem.js
+++ src/Component/ListItem/ListItem.js
@@ -1,12 +1,13 @@
 import react from "react";
 import Button from "../Button/Button";
 
-const ListItem = ({ todo, index, value, removeTodo, editTodo }) => {
+const ListItem = ({ todo, index, removeTodo, editTodo }) => {
   return (
     <div className="list-item">
       <span>{todo.value}</span>
       <div className="list-item-button">
         <Button onclick={() => removeTodo(index)}>X</Button>
+        <Button onclick={editTodo}>E</Button>
       </div>
     </div>
   );
