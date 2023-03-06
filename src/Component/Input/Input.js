ff5b8e836e73b1e85eec9301b41033af39ba8c40
AhmedHassan588 <syedahmedhassan588@gmail.com>
2023-03-06 14:22:58 +0500

    Update-Input

--- src/Component/Input/Input.js
+++ src/Component/Input/Input.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-const Input = ({ children, onChange, value }) => {
+const Input = ({ children, onChange, value, label, placeholder }) => {
   return (
-    <input value={value} onChange={onChange}>
+    <input
+      label={label}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    >
       {children}
     </input>
   );
