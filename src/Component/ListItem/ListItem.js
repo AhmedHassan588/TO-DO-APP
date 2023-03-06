96776add30a18872f34ae7952c4c9326c3987e62
AhmedHassan588 <syedahmedhassan588@gmail.com>
2023-03-06 14:23:16 +0500

    List_item

--- src/Component/ListItem/ListItem.js
+++ src/Component/ListItem/ListItem.js
@@ -1,13 +1,15 @@
 import react from "react";
 import Button from "../Button/Button";
+import { MdDelete } from "react-icons/md";
+import { FiEdit } from "react-icons/fi";
 
 const ListItem = ({ todo, index, removeTodo, editTodo }) => {
   return (
     <div className="list-item">
       <span>{todo.value}</span>
       <div className="list-item-button">
-        <Button onclick={() => removeTodo(index)}>X</Button>
-        <Button onclick={editTodo}>E</Button>
+        <Button onClick={() => removeTodo(index)}><MdDelete /></Button>
+        <Button onClick={editTodo}><FiEdit /></Button>
       </div>
     </div>
   );
